4cfb0801fff4e4f5776695d53fd731b02ba78802
Kerem Mert <kerem37mert@yahoo.com>
2023-02-17 15:14:15 +0300

    header footer php files created

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="footer-logo">
+        <a class="footer-link" href="https://www.keremmert.net">Keremmert.net</a>
+    </div>
+    <div class="footer-router">
+        <a class="footer-navigation" href="/">Gizlilik Politikası</a>
+        <a class="footer-navigation" href="/">Kullanım Koşulları</a>
+        <a class="footer-navigation" href="/">Yardım</a>
+    </div>
+</footer>
+
+<script src="javascript/main.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<!doctype html>
-<html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <header>
-        <div class="logo-container">
-            <a href="/">
-                <img class="logo" src="images/logo.png" alt="Bilgi Oyunu" />
-            </a>
-            <a class="open-menu" href="#">
-                <svg viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Menu</title> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Menu"> <rect id="Rectangle" fill-rule="nonzero" x="0" y="0" width="24" height="24"> </rect> <line x1="5" y1="7" x2="19" y2="7" id="Path" stroke="#ffffff" stroke-width="2" stroke-linecap="round"> </line> <line x1="5" y1="17" x2="19" y2="17" id="Path" stroke="#ffffff" stroke-width="2" stroke-linecap="round"> </line> <line x1="5" y1="12" x2="19" y2="12" id="Path" stroke="#ffffff" stroke-width="2" stroke-linecap="round"> </line> </g> </g> </g></svg>
-            </a>
-            <a class="close-menu" href="#">
-                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#ffffff"></path></g></svg>            </a>
-        </div>
-        <nav class="navbar">
-        <ul>
-            <li><a href="">Ana Sayfa</a></li>
-            <li><a href="">Haberler</a></li>
-            <li><a href="">Soru Oluştur</a></li>
-            <li><a href="">Yardım</a></li>
-        </ul>
-        </nav>
-    </header>
+<?php require_once("header.php"); ?>
     <main>
         <section class="download">
             <div class="phone-container">
@@ -40,17 +11,4 @@
             </div>
         </section>
     </main>
-    <footer class="footer">
-        <div class="footer-logo">
-            <a class="footer-link" href="https://www.keremmert.net">Keremmert.net</a>
-        </div>
-        <div class="footer-router">
-            <a class="footer-navigation" href="/">Gizlilik Politikası</a>
-            <a class="footer-navigation" href="/">Kullanım Koşulları</a>
-            <a class="footer-navigation" href="/">Yardım</a>
-        </div>
-    </footer>
-
-    <script src="javascript/main.js"></script>
-</body>
-</html>
+    <?php require_once("footer.php");
