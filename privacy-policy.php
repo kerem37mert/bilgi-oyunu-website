908ee0dd3f41cf0c2a632eeeddafa752c88fc725
Kerem Mert <kerem37mert@yahoo.com>
2023-02-22 14:08:09 +0300

    privacy policy complete

---
+++ privacy-policy.php
@@ -0,0 +1,75 @@
+<?php
+require_once("header.php");
+?>
+    <section class="content-section">
+        <article class="content-article">
+            <div class="content-header">
+                <h2>Gizlilik Politikası</h2>
+            </div>
+            <hr>
+            <div>
+                <p style="text-align:justify"><strong>GİZLİLİK VE KİŞİSEL VERİLERİ KORUMA POLİTİKASI</strong></p>
+                <p style="text-align:justify">keremmert.net olarak kişisel verilerinizin g&uuml;venliği hususuna azami hassasiyet g&ouml;stermekteyiz. Kişisel verileriniz 6698 sayılı Kişisel Verilerin Korunması Kanunu (&ldquo;KVKK&rdquo;)&rsquo;na uygun olarak işlenmekte ve muhafaza edilmektedir.&nbsp;</p>
+                <p style="text-align:justify">1.&nbsp;<strong>Kişisel Verilerinizin Ne Şekilde İşlenebileceği</strong></p>
+                <p style="text-align:justify">6698 sayılı KVKK uyarınca, keremmert.net ile paylaştığınız kişisel verileriniz, tamamen veya kısmen, otomatik olarak veyahut herhangi bir veri kayıt sisteminin par&ccedil;ası olmak kaydıyla otomatik olmayan yollarla elde edilerek, kaydedilerek, depolanarak, değiştirilerek, yeniden d&uuml;zenlenerek, kısacası veriler &uuml;zerinde ger&ccedil;ekleştirilen her t&uuml;rl&uuml; işleme konu olarak tarafımızdan işlenebilecektir. KVKK kapsamında veriler &uuml;zerinde ger&ccedil;ekleştirilen her t&uuml;rl&uuml; işlem &ldquo;kişisel verilerin işlenmesi&rdquo; olarak kabul edilmektedir.</p>
+                <p style="text-align:justify">2.&nbsp;<strong>Kişisel Verilerinizin İşlenme Ama&ccedil;ları Ve Hukuki Sebepleri</strong></p>
+                <p style="text-align:justify">Paylaştığınız kişisel veriler,</p>
+                <ul>
+                    <li style="text-align: justify;">M&uuml;şterilerimize verdiğimiz hizmetlerin gereklerini, s&ouml;zleşmenin ve teknolojinin gereklerine uygun şekilde yapabilmek, sunulan &uuml;r&uuml;n ve hizmetlerimizi geliştirebilmek i&ccedil;in;</li>
+                    <li style="text-align: justify;">Kamu g&uuml;venliğine ilişkin hususlarda ve hukuki uyuşmazlıklarda, talep halinde ve mevzuat gereği savcılıklara, mahkemelere ve ilgili kamu g&ouml;revlilerine bilgi verebilmek i&ccedil;in;</li>
+                    <li style="text-align: justify;">&Uuml;yelerimize geniş kapsamda &ccedil;eşitli imk&acirc;nlar sunabilmek veya bu imk&acirc;nları sunabilecek kişi veya kurumlarla yasal &ccedil;er&ccedil;evede paylaşabilmek i&ccedil;in;</li>
+                    <li style="text-align: justify;">Reklam tercihlerini analiz etmek i&ccedil;in,</li>
+                </ul>
+                <p style="text-align:justify">6698 sayılı KVKK ve ilgili ikincil d&uuml;zenlemelere uygun olarak işlenecektir.</p>
+                <p style="text-align:justify">3.&nbsp;<strong>Kişisel Verilerinizin Aktarılabileceği &Uuml;&ccedil;&uuml;nc&uuml; Kişi Veya Kuruluşlar Hakkında Bilgilendirme</strong></p>
+                <p style="text-align:justify">Yukarıda belirtilen ama&ccedil;larla, keremmert.net ile paylaştığınız kişisel verilerinizin aktarılabileceği kişi / kuruluşlar; ana hissedarlarımız, hissedarlarımız, reklam verenler, doğrudan veya dolaylı yurt i&ccedil;i / yurt dışı iştiraklerimiz; başta keremmert.net altyapısını kullanan &uuml;ye firmalar ve bunlarla sınırlı olmamak &uuml;zere sunulan hizmet ile ilgili kişi ve kuruluşlar olmak &uuml;zere, faaliyetlerimizi y&uuml;r&uuml;tmek &uuml;zere ve/veya Veri İşleyen sıfatı ile hizmet aldığımız, iş birliği yaptığımız, program ortağı kuruluşları, yurti&ccedil;i / yurtdışı kuruluşlar ve diğer 3. kişiler ve kuruluşlardır.</p>
+                <p style="text-align:justify">Reklam ama&ccedil;lı kullanılacak &ccedil;erezlere ilişkin d&uuml;zenlemelerimiz &ldquo;keremmert.net &Ccedil;erez Politikası&rdquo; olup, şu anda okuduğunuz &ldquo;Gizlilik ve Kişisel Verilerin Korunması Politikası&rdquo;nın bir par&ccedil;asını oluşturmaktadır. &Ccedil;erez Politikamız hakkında bilgi edinmek i&ccedil;in l&uuml;tfen tıklayın.<em>&nbsp;(Gereken &ccedil;erez politikası &ouml;rneğine belgenin en altında bulunan bağlantı ile ulaşabilirsiniz)</em></p>
+                <p style="text-align:justify">4.&nbsp;<strong>Kişisel Verilerinizin Toplanma Şekli</strong></p>
+                <p style="text-align:justify">Kişisel verileriniz,</p>
+                <ul>
+                    <li style="text-align: justify;">keremmert.net internet sitesi ve mobil uygulamalarındaki formlar ile ad, soyad, adres, telefon, iş veya &ouml;zel e-posta adresi gibi bilgiler ile; kullanıcı adı ve şifresi kullanılarak giriş yapılan sayfalardaki tercihleri, ger&ccedil;ekleştirilen işlemlerin IP kayıtları, tarayıcı tarafından toplanan &ccedil;erez verileri ile gezinme s&uuml;re ve detaylarını i&ccedil;eren veriler, konum verileri şeklinde;</li>
+                    <li style="text-align: justify;">keremmert.net&nbsp;ile ticari ilişki kurmak, iş başvurusu yapmak, teklif vermek gibi ama&ccedil;larla, kartvizit, &ouml;zge&ccedil;miş (cv), teklif vermek ve sair yollarla kişisel verilerini paylaşan kişilerden alınan, fiziki veya sanal bir ortamda, y&uuml;z y&uuml;ze ya da mesafeli, s&ouml;zl&uuml; veya yazılı ya da elektronik ortamdan;</li>
+                    <li style="text-align: justify;">Ayrıca farklı kanallardan dolaylı yoldan elde edilen, web sitesi, blog, yarışma, anket, oyun, kampanya ve benzeri ama&ccedil;lı kullanılan (mikro) web sitelerinden ve sosyal medyadan elde edilen veriler, e-b&uuml;lten okuma veya tıklama hareketleri, kamuya a&ccedil;ık veri tabanlarının sunduğu veriler, sosyal medya platformları (Facebook, Twitter, Google, Instagram, Snapchat vs) gibi sosyal paylaşım sitelerinden paylaşıma a&ccedil;ık profil ve verilerden; işlenebilmekte ve toplanabilmektedir.</li>
+                </ul>
+                <p style="text-align:justify">5.&nbsp;<strong>KVKK Y&uuml;r&uuml;rl&uuml;ğe Girmeden &Ouml;nce Elde Edilen Kişisel Verileriniz</strong></p>
+                <p style="text-align:justify">keremmert.net, KVKK&rsquo;nun y&uuml;r&uuml;rl&uuml;k tarihi olan 7 Nisan 2016 tarihinden &ouml;nce yayım hayatına başlamadığı i&ccedil;in bu tarihten &ouml;nce depoladığımız bir kişisel veri bulunmamaktadır.&nbsp;<em>(Eğer web siteniz bu tarihten &ouml;nce de veri topluyorsa verilerin ne şekilde işlendiği ve muhafaza edildiği mutlaka belirtilmelidir.)</em></p>
+                <p style="text-align:justify">6.&nbsp;<strong>Kişisel Verilerinizin Aktarılması</strong></p>
+                <p style="text-align:justify">T&uuml;rkiye&rsquo;de işlenerek veya T&uuml;rkiye dışında işlenip muhafaza edilmek &uuml;zere, yukarıda sayılan y&ouml;ntemlerden herhangi birisi ile toplanmış kişisel verileriniz KVKK kapsamında kalmak kaydıyla ve s&ouml;zleşme ama&ccedil;larına uygun olarak yurtdışında bulunan (Kişisel Veriler Kurulu tarafından akredite edilen ve kişisel verilerin korunması hususunda yeterli korumanın bulunduğu &uuml;lkelere) hizmet aracılarına da aktarılabilecektir.</p>
+                <p style="text-align:justify">Kişisel Verileriniz;</p>
+                <ul>
+                    <li style="text-align: justify;">&Uuml;r&uuml;n ve hizmetlerin sunulması ve tanıtılması i&ccedil;in işbirliği yapılan ve/veya hizmet alınan iş ortaklarımız,</li>
+                    <li style="text-align: justify;">Acil yardım &ccedil;ağrısı halinde konumunuzu tespit edecek olan yetkili mercilere,</li>
+                    <li style="text-align: justify;">D&uuml;zenleyici ve denetleyici kurumlar ile mahkeme ve icra m&uuml;d&uuml;rl&uuml;kleri gibi sair resmi kurumlara, kişisel verilerinizi talep etmeye yetkili olan diğer kamu kurum veya kuruluşlarına,</li>
+                    <li style="text-align: justify;">keremmert.net ile ticari ilişki i&ccedil;inde bulunan ve telefon numaranıza sahip t&uuml;zel kişilere,</li>
+                </ul>
+                <p style="text-align:justify">Gerekli g&ouml;r&uuml;ld&uuml;ğ&uuml; durumlarda aktarılabilecektir.</p>
+                <p style="text-align:justify">7.&nbsp;<strong>Kişisel Verilerin Saklanması Ve Korunması</strong></p>
+                <p style="text-align:justify">keremmert.net , kişisel verilerinizin barındığı sistemleri ve veri tabanlarını, KVKK&rsquo;nun 12. Maddesi gereği kişisel verilerin hukuka aykırı olarak işlenmesini &ouml;nlemekle, yetkisiz kişilerin erişimlerini engellemekle; muhafazalarını sağlamak amacıyla hash, şifreleme, işlem kaydı, erişim y&ouml;netimi gibi yazılımsal tedbirleri ve fiziksel g&uuml;venlik &ouml;nlemleri almakla m&uuml;kelleftir. Kişisel verilerin yasal olmayan yollarla başkaları tarafından elde edilmesinin &ouml;ğrenilmesi halinde durum derhal, yasal d&uuml;zenlemeye uygun ve yazılı olarak Kişisel Verileri Koruma Kurulu&rsquo;na bildirilecektir.</p>
+                <p style="text-align:justify">8.&nbsp;<strong>Kişisel Verilerin G&uuml;ncel Ve Doğru Tutulması</strong></p>
+                <p style="text-align:justify">KVKK&rsquo;nun 4. maddesi uyarınca keremmert.net&rsquo;in kişisel verilerinizi doğru ve g&uuml;ncel olarak tutma y&uuml;k&uuml;ml&uuml;l&uuml;ğ&uuml; bulunmaktadır. Bu kapsamda keremmert.net&#39;in y&uuml;r&uuml;rl&uuml;kteki mevzuattan doğan y&uuml;k&uuml;ml&uuml;l&uuml;klerini yerine getirebilmesi i&ccedil;in &uuml;yelerimizin doğru ve g&uuml;ncel verilerini paylaşması veya web sitesi / mobil uygulama &uuml;zerinden g&uuml;ncellemesi gerekmektedir.</p>
+                <p style="text-align:justify">9.&nbsp;<strong>6698 Sayılı Kvkk Uyarınca Kişisel Veri Sahibinin Hakları</strong></p>
+                <p style="text-align:justify">6698 sayılı KVKK 11.maddesi 07 Ekim 2016 tarihinde y&uuml;r&uuml;rl&uuml;ğe girmiş olup ilgili madde gereğince, Kişisel Veri Sahibi&rsquo;nin bu tarihten sonraki hakları aşağıdaki gibidir:</p>
+                <ul>
+                    <li style="text-align: justify;">Kişisel Veri Sahibi, keremmert.net&#39;e&nbsp;(veri sorumlusu) başvurarak kendisiyle ilgili;</li>
+                    <li style="text-align: justify;">Kişisel veri işlenip işlenmediğini &ouml;ğrenme,</li>
+                    <li style="text-align: justify;">Kişisel verileri işlenmişse buna ilişkin bilgi talep etme,</li>
+                    <li style="text-align: justify;">Kişisel verilerin işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını &ouml;ğrenme,</li>
+                    <li style="text-align: justify;">Yurt i&ccedil;inde veya yurt dışında kişisel verilerin aktarıldığı &uuml;&ccedil;&uuml;nc&uuml; kişileri bilme,</li>
+                    <li style="text-align: justify;">Kişisel verilerin eksik veya yanlış işlenmiş olması h&acirc;linde bunların d&uuml;zeltilmesini isteme,</li>
+                    <li style="text-align: justify;">KVKK&rsquo;nun 7. maddesinde &ouml;ng&ouml;r&uuml;len şartlar &ccedil;er&ccedil;evesinde kişisel verilerin silinmesini veya yok edilmesini isteme,</li>
+                    <li style="text-align: justify;">Kişisel verilerin d&uuml;zeltilmesi, silinmesi, yok edilmesi halinde bu işlemlerin, kişisel verilerin aktarıldığı &uuml;&ccedil;&uuml;nc&uuml; kişilere de bildirilmesini isteme,</li>
+                    <li style="text-align: justify;">İşlenen verilerin m&uuml;nhasıran otomatik sistemler vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya &ccedil;ıkmasına itiraz etme,</li>
+                    <li style="text-align: justify;">Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması h&acirc;linde zararın giderilmesini talep etme, haklarına sahiptir.</li>
+                </ul>
+                <p style="text-align:justify">10.&nbsp;<strong>İletişim ve Başvuru Y&ouml;ntemi</strong></p>
+                <p style="text-align:justify">keremmert.net tarafından atanacak Veri Sorumlusu Temsilcisi, yasal altyapı sağlandığında Veri Sorumluları Sicilinde ve bu belgenin bulunduğu internet adresinde ilan edilecektir.</p>
+                <p style="text-align:justify">Kişisel Veri Sahipleri, sorularını, g&ouml;r&uuml;şlerini veya taleplerini merhaba@keremmert.net e-posta adresine y&ouml;neltebilirler.</p>
+                <p style="text-align:justify">keremmert.net iletilen taleplere, gerek&ccedil;eli olmak ve 30 g&uuml;n i&ccedil;inde cevap vermek kaydıyla olumlu/olumsuz yanıtını, yazılı veya dijital ortamdan verebilir. Taleplere ilişkin gerekli işlemlerin &uuml;cretsiz olması esastır. Ancak işlemlerin bir maliyet gerektirmesi halinde, keremmert.net , &uuml;cret talebinde bulunma hakkını saklı tutar. Bu &uuml;cretler, Kişisel Verilerin Korunması Kurulu tarafından, Kişisel Verilerin korunması Kanunu&rsquo;nun 13. maddesine g&ouml;re belirlenen tarife &uuml;zerinden belirlenir.</p>
+                <p style="text-align:justify">Web sayfamızda, uygulamalarımızda ve diğer sair kanallarımızda kişisel verilerinizi paylaşarak Kişisel Veriler Politikamızı ve politikamızda yer alan işlenme, işlenme y&ouml;ntemleri, verilerin aktarılması, satışı ve diğer ilgili hususlar hakkındaki şartları, Bir sosyal medya uygulaması olan keremmert.net ile paylaşılan verilerin web sayfasında, uygulamalarda ve sosyal medya kanallarında kullanılmasını, bildirimlerde ve &ouml;nerilerde bulunulmasını, &uuml;yelerin yararına olması şartıyla ticari anlamda &uuml;&ccedil;&uuml;nc&uuml; kişilerle paylaşılabileceğini ve yine bunun i&ccedil;in kabulde bulunduğunuzu, yasal haklarınızı kullanmadan &ouml;nce keremmert.net&#39;e başvuruda bulunacağınızı KVKK&rsquo;da b&uuml;y&uuml;k &ouml;neme haiz, belirli bir konuya ilişkin, bilgilendirilmeye dayanan ve &ouml;zg&uuml;r iradeyle a&ccedil;ıklanan rıza şeklinde tanımlanan&nbsp;<strong>a&ccedil;ık bir rıza</strong>&nbsp;ile kabul ettiğinizi beyan etmiş olursunuz.</p>
+                <p style="text-align:justify"><em>Bu s&ouml;zleşme en son 22/02/2023 tarihinde g&uuml;ncellenmiştir.</em></p>
+                <p>Bu s&ouml;zleşmenin bir par&ccedil;asını oluşturan &ccedil;erez politikası &ouml;rneğine&nbsp;<a href="https://www.keremmert.net/cerez-politikasi" rel="noreferrer noopener" target="_blank">buradan</a>&nbsp;ulaşabilirsiniz.</p>
+            </div>
+        </article>
+    </section>
+<?php
+require_once("footer.php");
