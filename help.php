be8c1e4577dd6adfa8e5d9c0470a4b518ffc4094
Kerem Mert <kerem37mert@yahoo.com>
2023-02-22 12:38:54 +0300

    help pahe end

---
+++ help.php
@@ -0,0 +1,11 @@
+<?php
+require_once("header.php");
+?>
+    <section class="help-section">
+        <p>
+            Yardıma mı ihtiyacınız var? Hemen bizimle iletişime geçin.
+        </p>
+        <a href="mailto:bilgioyunu@keremmert.net">Mail Gönder</a>
+    </section>
+<?php
+require_once("footer.php");
